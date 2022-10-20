bd0c8aa8d3726284370aeea3622e5108f1ef73be
Faiza Javed <faizafj@outlook.com>
2022-10-20 21:24:31 +0100

    Completed 4/5 basic assessment tasks and all but one basic excerise

--- PracticeProgramming/src/main/java/com/sg/foundations/summative/assessments/DogGenetics.java
+++ PracticeProgramming/src/main/java/com/sg/foundations/summative/assessments/DogGenetics.java
@@ -1,27 +1,33 @@
 package com.sg.foundations.summative.assessments;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 import java.util.Random;
 
 public class DogGenetics {
     public static void main(String[] args) {
         Random randomPercentage = new Random (); //initialising the randomPercentage variable and using the random module.
-        int percentages = 0; 
-        
         String [] dogList = {"St. Bernard", "Chihuahua", "Dramatic RedNosed Asian Pug", "Common Cur", "King Doberman" }; //A list with all the dog types
-        
         int dogListLength = dogList.length;
-        for (int i = 0; i < dogListLength; i++){
-            System.out.println(dogList[i]);
-            int percentage = randomPercentage.nextInt(1, 100); 
-            percentages +=  percentage;
+        int totalPercentage = 100; 
+        
+        for (int i = 0; i < dogListLength; i++) { //for every 'number' from 0 to the length of the list;
+            System.out.println(dogList[i]); //print out the dog name in index position i.
+
+            if (totalPercentage <= 100) {
+
+                if (i == dogListLength - 1) { //if it's the last element
+                    totalPercentage = 100 - totalPercentage; //the percentage will be 100 - total percentage
+                    System.out.println(totalPercentage);
+                } else {
+                    int percentage = randomPercentage.nextInt(totalPercentage);  //select a random number between 1 and the total percentages.
+                    totalPercentage = totalPercentage - percentage;
+                    System.out.println(totalPercentage);
+                }
+            } else {
+                System.out.println("0%");
+            }
             
-            System.out.println(percentage);
-            
+
         }
     }
 }
+
